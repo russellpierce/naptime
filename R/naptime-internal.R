442c46c379604b4fd497e69339522d1bbbb53a64
Russell Pierce <russell.pierce@zapier.com>
2016-11-13 18:51:46 -0600

    permissive as option and arg

--- R/naptime-internal.R
+++ R/naptime-internal.R
@@ -1,11 +1,12 @@
+permissive_default <- FALSE
 nap_default <- function() naptime(getOption("naptime.default_delay", 0.1))
 nap_warn <- function(...) {
   if (getOption("naptime.warnings", TRUE)) {
     warning(..., call. = FALSE)
   }
 }
-nap_error <- function(...) {
- if (getOption("naptime.permissive", FALSE)) {
+nap_error <- function(..., permissive = getOption("naptime.permissive", permissive_default)) {
+ if (permissive) {
    nap_warn(...)
  } else {
    stop(..., call. = FALSE)
--- R/naptime.R
+++ R/naptime.R
@@ -4,14 +4,19 @@
 #' \itemize{
 #'  \item numeric: time in seconds to nap
 #'  \item POSIXct: time at which the nap should stop  (timezone is respected)
-#'  \item Period: time from now at which the nap should stop
 #'  \item character: yyyy-mm-dd hh:mm:ss at which nap should stop, time zone is assumed to be Sys.timezone() and hh:mm:ss is optional as three formats may be missing, cf. lubridate::ymd_hms().
+#'  \item Period: time from now at which the nap should stop
 #'  \item difftime: difference in time to nap
-#'  \item logical: nap for default duration
-#'  \item NULL: nap for default duration
-#'  \item generic: nap for default duration
+#'  \item logical: If TRUE, nap for default duration, otherwise don't nap.
+#'  \item NULL: don't nap
+#'  \item generic: error or nap for default duration depending on the option naptime.permissive
 #' }
+#'
+#' The default duration is set with a numeric for the option \code{naptime.default_delay} in seconds (default: 0.1)
+#' Whether a generic input is accepted is determined by the option \code{naptime.permissive} (default: FALSE)
+#'
 #' @param time Time to sleep, polymorphic type inputs, leaning towards units as 'seconds'
+#' @param permissive An optional argument to override the \code{naptime.permissive option} for this call of the naptime function
 #' @rdname naptime
 #'
 #' @return NULL; A side effect of a pause in program execution
@@ -25,97 +30,113 @@
 #' }
 
 setGeneric("naptime",
-           function(time)
+           function(time, permissive = getOption("naptime.permissive", permissive_default))
            {
              if (missing(time)) {
-               time <- getOption("naptime.default_delay", 0.1)
+               nap_default()
+             } else if (!is.null(time) && length(time) != 1L) {
+               nap_error("The time paramater was not scalar (length equal to 1)", permissive = permissive)
+               if (length(time) > 0) {
+                 naptime(time[1], permissive = permissive)
+               } else {
+                 naptime(permissive = permissive)
+               }
+             } else {
+               tryCatch(
+                 standardGeneric("naptime")
+                 , error = function(e) {
+                   nap_error("unhandled input for naptime(): ", as.character(e), permissive = permissive)
+                   nap_default()
+                 }
+               )
              }
-             tryCatch(
-              standardGeneric("naptime")
-              , error = function(e) {
-                nap_warn("unhandled input for naptime: ", as.character(e), "; sleeping for default duration")
-                nap_default()
-              }
-             )
              return(NULL)
            })
 
 #' @rdname naptime
 setMethod("naptime", signature("numeric"),
-          function(time)
+          function(time, permissive = getOption("naptime.permissive", permissive_default))
           {
-            if (is.finite(time) && time >= 0)
+            if (is.finite(time) && time >= 0) {
               Sys.sleep(time)
-            else {
-              nap_warn('Non-finite or negative time passed to naptime(), sleeping for default duration')
+            } else if (!is.finite(time) && time > 0) {
+              nap_error("naptime() provided with a time that was positive and non-finite", permissive = permissive)
               nap_default()
+            } else {
+              nap_warn("naptime() provided with a time that was negative or in the past, skipping delay")
             }
           })
 
 #' @rdname naptime
 setMethod("naptime", signature("Period"),
-          function(time)
+          function(time, permissive = getOption("naptime.permissive", permissive_default))
           {
-            t <- lubridate::period_to_seconds(time)
-            if (t < 0) {
-              nap_warn('Time interval is less than zero, sleeping for .Options$naptime.default_delay seconds.')
-              t <- getOption("naptime.default_delay")
-            }
-            Sys.sleep(t)
+            naptime(lubridate::period_to_seconds(time), permissive = permissive)
           })
 
 #' @rdname naptime
 setMethod("naptime", signature("POSIXct"),
-          function(time)
+          function(time, permissive = getOption("naptime.permissive", permissive_default))
           {
             t <- as.numeric(time) - as.numeric(lubridate::now(tzone = lubridate::tz(time)))
-            naptime(t)
+            naptime(t, permissive = permissive)
           })
 
 #' @rdname naptime
 setMethod("naptime", signature("difftime"),
-          function(time)
+          function(time, permissive = getOption("naptime.permissive", permissive_default))
           {
-            t <- as.numeric(time)
-            if (t < 0) {
-              nap_warn('Time interval is less than zero.')
-              t <- 0
-            }
-            Sys.sleep(t)
+            #Use the units of difftime to construct a Period
+            secs <- lubridate::seconds
+            naptime(
+              eval(
+                parse(
+                  text=paste0(attr(time, "units"), "(", as.numeric(time), ")")
+                  )
+                )
+            , permissive = permissive)
           })
 
 #' @rdname naptime
 setMethod("naptime", signature("logical"),
-          function(time)
+          function(time, permissive = getOption("naptime.permissive", permissive_default))
           {
-            nap_warn('Logical passed to naptime(), sleeping for default duration')
-            nap_default()
+            if (isTRUE(time)) {
+              nap_default()
+            } else {
+              if (isTRUE(!time)) {
+                NULL
+              } else {
+                nap_error("Logical provided to naptime() is not TRUE or FALSE.")
+                nap_default()
+              }
+            }
           })
 
 #' @rdname naptime
 setMethod("naptime", signature("NULL"),
-          function(time)
+          function(time, permissive = getOption("naptime.permissive", permissive_default))
           {
-            nap_warn('NULL passed to naptime(), sleeping for default duration')
-            nap_default()
+            NULL
           })
 
 #' @rdname naptime
 setMethod("naptime", signature("character"),
-          function(time)
+          function(time, permissive = getOption("naptime.permissive", permissive_default))
           {
             time_zone <- ifelse(is.na(Sys.timezone()), "UTC", Sys.timezone())
-            if (nchar(time) >= 8) {
-              time_parsed <- try(lubridate::ymd_hms(time, tz = time_zone, truncated = 3), silent = TRUE)
-            } else {
+            num_char <- nchar(time)
+            if (is.na(num_char) || num_char < 8) {
               # Times that aren't at least 8 characters long do not have a reasonable chance of being parsable
               time_parsed <- NA
+            } else if (num_char >= 8) {
+              time_parsed <- try(lubridate::ymd_hms(time, tz = time_zone, truncated = 3), silent = TRUE)
             }
             if ("try-error" %in% class(time_parsed) || is.na(time_parsed)) {
-              nap_warn("Could not parse ", time, " as time, sleeping for default duration.")
+              nap_error("Could not parse '", time, "' as time")
               nap_default()
             } else {
               t <- time_parsed - lubridate::now(tzone = lubridate::tz(time_parsed))
-              naptime(t)
+              naptime(t, permissive = permissive)
             }
           })
